aae4fa76cb1319ca1f0e59c381b18b812cb7f498
timofeymac <timsidorin@gmail.com>
2025-02-24 22:01:27 +1000

    Задание1.py

---
+++ Задание1.py
@@ -0,0 +1,60 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return x**2 + math.sin(0.5 * x) - 1
+
+def chord_method(a, b, eps):
+    if f(a) * f(b) >= 0:
+        raise ValueError("На интервале нет корня")
+    while abs(b-a) > eps:
+        a = a - (b - a) * f(a) / (f(b) - f(a))
+        b = b - (a - b) * f(b) / (f(a) - f(b))
+        print(b)
+
+    return b
+
+
+a = 0.5
+b = 1
+eps = 0.001
+
+# Для построения графика
+x = np.arange(-5, 5, 0.2)
+
+y1 = x**2  # 1 часть
+y2 =  1 - np.sin(0.5*x)   # 2 часть
+y3 = x**2 + np.sin(0.5 * x) - 1  # весь график
+
+# расчет методом хорд
+root = chord_method(a, b, eps)
+
+# Создание фигуры с определенным размером
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# Построение графиков с метками для легенды
+plt.plot(x, y1, label='x²', linewidth=2.0)
+plt.plot(x, y2, label='sin(0.5x-1)', linewidth=2.0)
+plt.plot(x, y3, label='x² + sin(0.5x) - 1 (исходная функция)', linewidth=2.0)
+
+# Добавление точки пересечения с осью X
+plt.plot(root, 0, 'ro', markersize=10, label='Корень')
+
+plt.grid(True)
+plt.xlabel('Ось х')
+plt.axhline(y=0, color='black', linewidth=1.5)  # Способ 2
+plt.ylabel('Ось y')
+plt.title('1 задание')
+
+# Добавление легенды
+plt.legend()
+
+# Вывод значений корня под графиком
+fig.text(0.15, 0.02, f"Корень: {root:.3f}", fontsize=16)
+
+# Настройка отступов
+plt.tight_layout()
+plt.subplots_adjust(bottom=0.15)  # Оставляем место для текста внизу
+
+plt.show()
