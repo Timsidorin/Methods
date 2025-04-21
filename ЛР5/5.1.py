e6a671974bed72b16757a3dfb3b81c57e81276c5
timsidorin <timsidorin@gmail.com>
2025-04-21 20:18:36 +0700

    ГОТОВО

--- ЛР5/5.1.py
+++ ЛР5/5.1.py
@@ -8,7 +8,7 @@ a = 0.5
 b = 2.3
 n = 5 # Для примера
 precision = 0.001
-
+max_iterations = 20
 
 
 intervals = np.linspace(a, b, n+1) # отрезки
@@ -22,22 +22,25 @@ def f(x):
     return np.round(1/(x**2 + 2)**0.5, 4)
 
 
-#
-#
-# print("Расчётная таблица:\n")
-# print("i\tx_i\t\tf(x_i)")
-# print("------------------")
-# for i in range(n+1):
-#     print(f"{i}|\t{intervals[i]}|\t\t{f(intervals[i])}")
-#
-#
 
 
+print("Расчётная таблица:\n")
+print("i\tx_i\t\tf(x_i)")
+print("------------------")
+for i in range(n+1):
+    print(f"{i}|\t{intervals[i]}|\t\t{f(intervals[i])}")
 
-for _ in range(100):
+
+prev_formula = None
+
+print("\n")
+for _ in range(max_iterations):
     h = (b - a) / n
-    intervals = np.linspace(a, b, n + 1) # отрезки
-    f_x_i =  (f(intervals))
-    n*=2
-    formula = h * ((intervals[0]+intervals[-1]/2) + sum(f_x_i))
-    print(formula)
+    intervals = np.linspace(a, b, n + 1)
+    f_x_i = f(intervals)
+    current_formula = h * ((f_x_i[0] + f_x_i[-1]) / 2 + sum(f_x_i[1:-1]))
+    if prev_formula is not None and abs(current_formula - prev_formula) < precision:
+        break
+    print(f"Ответ: {np.round(current_formula, 5)}")
+    prev_formula = current_formula
+    n *= 2
